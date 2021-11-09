1d7f59af3b6f82fd5490c33a2fdcf0000f556f15
Ernesto Tham <ernestotha@gmail.com>
2021-11-08 23:57:59 -0500

    added generatePassword function and functions to get ramdom leters upper lower num or special chars

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,14 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function ramdomUpper(){
+  var UpperLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  return UpperLetter[Math.floor(Math.random()*UpperLetter.length)];
+}
 
+function ramdomLower(){
+  var LowerLetter = 'abcdefghijklmnopqrstuvwxyz'
+  return LowerLetter[Math.floor(Math.random()*LowerLetter.length)];
+
+}
+
+function ramdomNumber(){
+  var Num = '0123456789'
+  return Num[Math.floor(Math.random()*Num.length)];
+
+}
+
+function specialChar(){
+  var SpecialChar = '!@#$%^&*()_+-={}[]:"\';<>?,./`~'
+  return SpecialChar[Math.floor(Math.random()*SpecialChar.length)];
+}
 
 
 function generatePassword(){
 
 
-  var pwdLength = window.prompt("Enter password length");
+  var pwdLength = Number(window.prompt('Enter password length','8-128'));
+    if (pwdLength === null){
 
+      return;
+    }
+    
+    else {
+
+      while (pwdLength < 8 || pwdLength > 128) {
+         
+        if(pwdLength === 0){break;}
+
+        else{
+              console.log("hello " +pwdLength)
+              console.log("type is : " +typeof pwdLength)
+              window.alert("Length can only be between 8-128");
+              pwdLength = Number(window.prompt('Enter password length', '8-128'));
+            } //end else condition
+       }//end while loop
+   
+    
+
+    }
+
+    
 
 
 
